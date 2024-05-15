c9e0fcdb36db915ff601ca07fa23af4de8622fd8
NikosProtonotarios <147170230+NikosProtonotarios@users.noreply.github.com>
2024-05-15 21:05:44 +0100

    Add a function that makes the keyboard alive

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-let drumLength = document.querySelectorAll(".drum").length;
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (let i = 0; i < drumLength; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-       console.log(this.style.color = "white");
+for (let i = 0; i < numberOfDrumButtons; i++) {
+     document.querySelectorAll("button")[i].addEventListener("click", function() {
+      
+     let thisButton = this.innerHTML;
+     
+     makeSound(thisButton);
+
     });
 }
 
+document.addEventListener("keydown", function(event) {
+     makeSound(event.key);
+})
 
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
 
+        case "a":
+            let kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
 
-// var audio = new Audio('sounds/tom-1.mp3');
-// audio.play();
+        case "s":
+            let tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        
+        case "d":
+            let snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+            
+        case "j":
+            let crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "k":
+            let tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "l":
+            let tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();    
+            break;
+            
+            default: console.log(thisButton);
+                break;
+     }
+}
